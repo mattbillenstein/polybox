3b80c1cec4c60bd9b29af970d84927ee0e3e9e36
Matt Billenstein <matt@vazor.com>
2021-03-20 16:07:20 +0000

    Make install script idempotent

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+set -eo pipefail
+
+PAAS_USERNAME="polybox"
+REPO="mardix/polybox"
+BRANCH="master"
 
 echo "Polybox installer"
 
-apt-get update
-apt-get install -y bc wget \
+sudo apt-get update
+sudo apt-get install -y \
+   bc \
    curl \
    software-properties-common \
    cron \
@@ -31,30 +39,29 @@ apt-get install -y bc wget \
    nodejs \
    npm \
    nodeenv
-apt-get update
-
-PAAS_USERNAME=polybox
 
 # Create user
+id -u $PAAS_USERNAME &>/dev/null || \
 sudo adduser --disabled-password --gecos 'PaaS access' --ingroup www-data $PAAS_USERNAME
 
 # copy your public key to /tmp (assuming it's the first entry in authorized_keys)
 head -1 ~/.ssh/authorized_keys > /tmp/pubkey
 # install polybox and have it set up SSH keys and default files
-sudo su - $PAAS_USERNAME -c "wget https://raw.githubusercontent.com/mardix/polybox/master/polybox.py && python3 ~/polybox.py init && python3 ~/polybox.py set-ssh /tmp/pubkey"
+sudo su - $PAAS_USERNAME -c "curl -s https://raw.githubusercontent.com/$REPO/$BRANCH/polybox.py > polybox.py && python3 ~/polybox.py init && python3 ~/polybox.py set-ssh /tmp/pubkey"
 rm /tmp/pubkey
 
-
+sudo rm -f /etc/uwsgi/apps-enabled/polybox.ini
 sudo ln /home/$PAAS_USERNAME/.polybox/uwsgi/uwsgi.ini /etc/uwsgi/apps-enabled/polybox.ini
 sudo systemctl restart uwsgi
 
 cd /tmp
-wget https://raw.githubusercontent.com/mardix/polybox/master/incron.conf
-wget https://raw.githubusercontent.com/mardix/polybox/master/nginx.conf
-wget https://raw.githubusercontent.com/mardix/polybox/master/index.polybox.html
-cp /tmp/nginx.conf /etc/nginx/sites-available/default
-cp /tmp/incron.conf /etc/incron.d/polybox
-cp /tmp/index.polybox.html /var/www/html
+curl -s https://raw.githubusercontent.com/$REPO/$BRANCH/incron.conf > incron.conf
+curl -s https://raw.githubusercontent.com/$REPO/$BRANCH/nginx.conf > nginx.conf
+curl -s https://raw.githubusercontent.com/$REPO/$BRANCH/master/index.polybox.html > index.polybox.html
+sudo cp /tmp/nginx.conf /etc/nginx/sites-available/default
+sudo cp /tmp/incron.conf /etc/incron.d/polybox
+sudo cp /tmp/index.polybox.html /var/www/html
+sudo systemctl restart nginx
 
 echo ""
 echo "Polybox installation complete!"
